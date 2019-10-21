d1b72b92f5a5e6ce91b8610ea4db72d66feed8c4
Daeho Han <devhoho@hoMac.local>
2019-10-21 15:11:02 +0900

    compose 스크립트 정리

--- ch03/compose.sh
+++ ch03/compose.sh
@@ -9,23 +9,47 @@ down_cmd="$cmd down"
 
 if [ "$#" -lt 1 ]; then
     echo "파라미터를 입력해주세요."
+    echo "$0 help 를 입력하여 명령어를 확인하세요"
     exit 1
 elif [ "$#" -gt 1 ]; then
     echo "파라미터가 너무 많습니다. "
+    echo "$0 help 를 입력하여 명령어를 확인하세요"
     exit 1
 fi
 args=("$@")
 
+help_func() {
+    echo "Usage: $0 [option]"
+    echo "----------- options -----------"
+    echo "up : swarm을 위한 컨테이너들 daemon으로 띄웁니다."
+    echo "down : 컨테이너들을 종료합니다."
+}
+
+print_and_exec() {
+    echo "-명령어 : $cmd"
+    ``$cmd` >&1`
+    echo ""
+}
+
+up_func() {
+    echo "################### compose up ###################"
+    print_and_exec up_cmd
+}
+
+down_func() {
+    echo "################### compose down ###################"
+    print_and_exec down_cmd
+}
+
 case ${args[0]} in
+    help)
+        help_func
+        ;;
     up)
-        echo "################### compose up ###################"
-        echo "COMMAND : $up_cmd"
-        `$up_cmd` >&1
+        up_func
         ;;
     down)
-        echo "################### compose down ###################"
-        echo "COMMAND : $down_cmd"
-        `$down_cmd` >&1
+        down_func
         ;;
     *)
         echo "지원하지 않는 명령어입니다."
